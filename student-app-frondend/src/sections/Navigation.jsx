95957e9abab0539f3a092556608db08df7af5ba5
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-02 09:11:45 +0300

    Worked on a responsive navbar

--- student-app-frondend/src/sections/Navigation.jsx
+++ student-app-frondend/src/sections/Navigation.jsx
@@ -1,13 +1,26 @@
-import  Navbar  from "react-bootstrap/Navbar";
-import { NavbarBrand } from "react-bootstrap";
+import  NavLink from 'react-bootstrap/NavLink';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navigation = () =>
-{
-    return(
-        <>
-        
-        </>
-    );
+
+const Navigation = () =>{
+  return (
+    <Navbar bg="dark" data-bs-theme="dark"  expand="lg" className="bg-body-tertiary">
+      <Container>
+      <Navbar.Brand id="basic-nav" >Student App</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+      <Nav className='me-auto' >
+      <NavLink href='#Home'>Home</NavLink>
+      <NavLink href='#add student'>Add Student</NavLink>
+      <NavLink href='#view students'>View Students</NavLink>
+      </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
 
 export default Navigation;
